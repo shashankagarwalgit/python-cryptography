554673e69da7a587ca9ccb22aedbd82e9d78de3f
Shashank Agarwal <162678882+shashankagarwalgit@users.noreply.github.com>
2022-08-28 13:46:52 +0530

    encryption_decyption code

---
+++ encrypt_decrypt.py
@@ -0,0 +1,33 @@
+from fileinput import filename
+import sys
+from cryptography.fernet import Fernet
+
+
+key ="l8U1Znglx5S8Hcg1OnrLN2xBq8zfg99eOlb46Aiftxo="
+
+f= Fernet(key)
+
+user_file= input("Enter your filename:- ")
+  
+with open(user_file , 'rb') as ef:
+    data = ef.read()
+
+selec_ch = input("Do you want to encrypt or decrypt (e/d)? ")
+
+if selec_ch == "e" :
+    edata = f.encrypt(data)
+    with open(user_file , 'wb' ) as ef:
+        ef.write(edata)
+        print("Encrypted Successfully!!")
+    
+elif selec_ch == "d":
+    ddata = f.decrypt(data)
+    with open(user_file , 'wb') as df:
+        df.write(ddata)
+        print("Decrypted Successfully")
+        
+else:
+    sys.exit("Wrong Choice!!")
+
+
+   
